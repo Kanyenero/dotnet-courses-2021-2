e883479d8df1539a0e06ebd09803dd445920e35e
Zakhar Khakhalin <khakhalinz@yandex.ru>
2021-09-29 00:22:41 +0300

    Tasks 1-4

---
+++ 02-arrays-and-methods/ArraysAndMethods/Task1/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int fixedSize = 6;
+            int[] generatedArray = new int[fixedSize];
+            int[] sortedArray = new int[fixedSize];
+
+            int minArrayValue;
+            int maxArrayValue;
+
+            generatedArray = GenerateArray(fixedSize);
+            PrintArray(generatedArray);
+
+            Console.WriteLine("~~~~~");
+
+            sortedArray = SortAndGetMinAndMaxValues(generatedArray, out minArrayValue, out maxArrayValue);
+            PrintArray(sortedArray);
+        }
+
+        static int[] GenerateArray(int arraySize)
+        {
+            int[] array = new int[arraySize];
+            Random rnd = new Random();
+
+            for(int i = array.GetLowerBound(0); i <= array.GetUpperBound(0); i++)
+            {
+                array[i] = rnd.Next(-100, 100);
+            }
+
+            return array;
+        }
+
+        static int[] SortAndGetMinAndMaxValues(int[] arrayToSort, out int minValue, out int maxValue)
+        {
+            // sorting
+            int temp = 0;
+
+            for(int i = 0; i < arrayToSort.Length - 1; i++)
+            {
+                for(int j = 0; j < arrayToSort.Length - i - 1; j++)
+                {
+                    if(arrayToSort[j] > arrayToSort[j + 1])
+                    {
+                        temp = arrayToSort[j];
+                        arrayToSort[j] = arrayToSort[j + 1];
+                        arrayToSort[j + 1] = temp;
+                    }
+                }
+            }
+
+            //min and max
+            minValue = arrayToSort[0];
+            maxValue = arrayToSort[arrayToSort.Length - 1];
+
+            return arrayToSort;
+        }
+
+        static void PrintArray(int[] array)
+        {
+            for(int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+        }
+    }
+}
