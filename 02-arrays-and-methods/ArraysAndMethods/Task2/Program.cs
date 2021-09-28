af0e7c77f91cff01725971436899105f87ccb94d
Zakhar Khakhalin <khakhalinz@yandex.ru>
2021-09-29 00:59:28 +0300

    Task 1-4

--- 02-arrays-and-methods/ArraysAndMethods/Task2/Program.cs
+++ 02-arrays-and-methods/ArraysAndMethods/Task2/Program.cs
@@ -6,7 +6,62 @@ namespace Task2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[,,] array = new int[4,3,2];
+
+            GenerateArray(ref array);
+            Print3DArray(array);
+
+            Console.WriteLine("~~~~~");
+
+            ReplacePositiveElementsWithZero(ref array);
+            Print3DArray(array);
+        }
+
+        static void ReplacePositiveElementsWithZero(ref int[,,] array)
+        {
+            for (int i = array.GetLowerBound(0); i <= array.GetUpperBound(0); i++)
+            {
+                for (int j = array.GetLowerBound(1); j <= array.GetUpperBound(1); j++)
+                {
+                    for (int k = array.GetLowerBound(2); k <= array.GetUpperBound(2); k++)
+                    {
+                        if(array[i, j, k] > 0)
+                        {
+                            array[i, j, k] = 0;
+                        }
+                    }
+                }
+            }
+        }
+
+        static void GenerateArray(ref int[,,] array)
+        {
+            Random rnd = new Random();
+
+            for (int i = array.GetLowerBound(0); i <= array.GetUpperBound(0); i++)
+            {
+                for (int j = array.GetLowerBound(1); j <= array.GetUpperBound(1); j++)
+                {
+                    for (int k = array.GetLowerBound(2); k <= array.GetUpperBound(2); k++)
+                    {
+                        array[i,j,k] = rnd.Next(-100, 100);
+                    }
+                }
+            }
+        }
+
+        static void Print3DArray(int[,,] array)
+        {
+            for (int i = array.GetLowerBound(0); i <= array.GetUpperBound(0); i++)
+            {
+                for(int j = array.GetLowerBound(1); j <= array.GetUpperBound(1); j++)
+                {
+                    for(int k = array.GetLowerBound(2); k <= array.GetUpperBound(2); k++)
+                    {
+                        Console.WriteLine(array[i,j,k]);
+                    }
+                }
+            }
         }
     }
 }
