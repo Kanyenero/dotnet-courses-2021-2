c662cf582935900d797c20893acf4f30fce1af8d
Zakhar Khakhalin <khakhalinz@yandex.ru>
2021-09-29 01:13:48 +0300

    Task 1-4

--- 02-arrays-and-methods/ArraysAndMethods/Task3/Program.cs
+++ 02-arrays-and-methods/ArraysAndMethods/Task3/Program.cs
@@ -6,7 +6,44 @@ namespace Task3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] array = new int[10];
+            GenerateArray(ref array);
+            PrintArray(ref array);
+
+            Console.WriteLine("~~~~~");
+
+            int sum = GetSumOfNonNegativeElements(array);
+            Console.WriteLine(sum);
+        }
+
+        static int GetSumOfNonNegativeElements(int[] array)
+        {
+            int sum = 0;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] > 0) { sum += array[i]; }
+            }
+
+            return sum;
+        }
+
+        static void GenerateArray(ref int[] array)
+        {
+            Random rnd = new Random();
+
+            for (int i = array.GetLowerBound(0); i <= array.GetUpperBound(0); i++)
+            {
+                array[i] = rnd.Next(-100, 100);
+            }
+        }
+
+        static void PrintArray(ref int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
         }
     }
 }
