6d5e6b7ff520c5017a5482000572c7596bb14b8c
Zakhar Khakhalin <khakhalinz@yandex.ru>
2021-09-29 17:24:22 +0300

    Task 1-7

---
+++ 03-strings/Strings/Task1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userInput = Console.ReadLine();
+            userInput = userInput.Trim(new char[] {' ', ',', '.'});
+
+            Console.WriteLine($"Your input: {userInput}");
+
+            string[] spltUserInput = userInput.Split
+                (
+                    new char[] {' ', ',', '.', '!', '?', 
+                                ':', ';', '(', ')', '[', 
+                                ']', '{', '}', '\\', '/'}, 
+                    StringSplitOptions.RemoveEmptyEntries
+                );
+
+            int sumWordLengths = 0;
+
+            foreach (string word in spltUserInput)
+            {
+                Console.WriteLine(word + "\t\t:\t " + word.Length);
+
+                sumWordLengths += word.Length;
+            }
+
+            int avgWordLength = sumWordLengths / spltUserInput.Length;
+            Console.WriteLine(avgWordLength);
+        }
+    }
+}
