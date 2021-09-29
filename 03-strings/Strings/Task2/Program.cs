826a2aa89bea45e69a4b1e34378f7ca32535e483
Zakhar Khakhalin <khakhalinz@yandex.ru>
2021-09-29 19:31:44 +0300

    Task 1-7

--- 03-strings/Strings/Task2/Program.cs
+++ 03-strings/Strings/Task2/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Text;
+using System.Linq;
 
 namespace Task2
 {
@@ -6,7 +8,31 @@ namespace Task2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Input 1'st string: ");
+            string userInput1 = Console.ReadLine();
+
+            Console.Write("Input 2'nd string: ");
+            string userInput2 = Console.ReadLine();
+
+            string targets = RemoveDuplicates(userInput2);
+
+            var sb = new StringBuilder(userInput1);
+
+            foreach (char c in targets)
+            {
+                if (userInput1.Contains(c))
+                {
+                    sb.Replace(c.ToString(), $"{c}{c}");
+                }
+            }
+
+            string finalStr = sb.ToString();
+            Console.WriteLine(finalStr);
+        }
+
+        public static string RemoveDuplicates(string input)
+        {
+            return new string(input.ToCharArray().Distinct().ToArray());
         }
     }
 }
--- 03-strings/Strings/Task3/Program.cs
+++ 03-strings/Strings/Task3/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace Task3
 {
@@ -6,7 +7,31 @@ namespace Task3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            CultureInfo en  = new CultureInfo("en");
+            CultureInfo ru  = new CultureInfo("ru");
+            CultureInfo inv = new CultureInfo("");
+
+            showCultureDifferences(ref ru, ref en);
+            showCultureDifferences(ref en, ref inv);
+            showCultureDifferences(ref ru, ref inv);
+        }
+
+        public static void showCultureDifferences(ref CultureInfo ci1, ref CultureInfo ci2)
+        {
+            string infoRow = String.Format("{0,2} vs {1,2}: {2, 4} | {3, 4} | {4, 4} " +
+                                           "| {5, 4} | {6, 20} | {7, 20} | {8, 10} | {9, 10}",
+                ci1, 
+                ci2,
+                ci1.NumberFormat.NumberDecimalSeparator,
+                ci2.NumberFormat.NumberDecimalSeparator,
+                ci1.NumberFormat.NumberDecimalSeparator,
+                ci2.NumberFormat.NumberDecimalSeparator,
+                ci1.DateTimeFormat.LongDatePattern,
+                ci2.DateTimeFormat.LongDatePattern,
+                ci1.DateTimeFormat.LongTimePattern,
+                ci2.DateTimeFormat.LongTimePattern);
+
+            Console.WriteLine(infoRow);
         }
     }
 }
