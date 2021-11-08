729bc0a7ec845f88615b8df4aeb0ad3444f216f1
Zakhar Khakhalin <khakhalinz@yandex.ru>
2021-11-08 09:33:19 +0300

    Task 1-3

--- 10-dotnet-basics/DotnetBasics/Task 3/Program.cs
+++ 10-dotnet-basics/DotnetBasics/Task 3/Program.cs
@@ -2,11 +2,22 @@
 
 namespace Task_3
 {
-    class Program
+    class Program 
     {
         static void Main(string[] args)
         {
-            
+            int[] numbers = { -5, 0, 10, -7, 1, 16, -4 };
+            int[] answer = RemoveNegativesFromArray(numbers);
+
+            foreach(int item in answer)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        static int[] RemoveNegativesFromArray(int[] arr)
+        {
+            return Array.FindAll(arr, x => x >= 0);
         }
     }
 }
