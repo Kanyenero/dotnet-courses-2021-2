db8f333ab1c623d8fe35b47c6088cded55a96ad5
Zakhar Khakhalin <khakhalinz@yandex.ru>
2021-12-07 22:28:31 +0300

    ASP.NET (MVC) Study Project

--- 17-asp-net-basics/MVC4StudentsDemo/Department.DAL/IStudentDAO.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-namespace Department.DAL
-{
-	public interface IStudentDAO
-	{
-		void Add(Entities.Student student);
-		System.Collections.Generic.IEnumerable<Entities.Student> GetList();
-	}
-}
--- 17-asp-net-basics/TestMvc2/TestMvc2/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TestMvc2.MvcApplication" Language="C#" %>
